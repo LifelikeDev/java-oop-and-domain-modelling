81a0f04b68686bdc424c9477bf1b4c474bfeb5da
Navian <primeguyy@gmail.com>
2021-11-07 21:16:49 +0000

    adding methods to PrivateClient

--- src/PrivateClient.java
+++ src/PrivateClient.java
@@ -1,2 +1,9 @@
-public class PrivateClient {
+public class PrivateClient extends Client {
+    public PrivateClient(String name, String id, ServiceLevel serviceLevel) {
+        super(name, id, serviceLevel);
+    }
+
+    public PrivateClient(String id, ServiceLevel serviceLevel) {
+        super(id, serviceLevel);
+    }
 }
